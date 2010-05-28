d80371d152b9ba794677b543070ebda58672ba52
William Hsu <w.hsu1@uq.edu.au>
2010-05-28 16:41:53 +1000

    Updated octave_test for TPAC graph output

--- octave/octavdiff.sh
+++ octave/octavdiff.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
 # because grisu-test.properties command parameter cannot do it?
+# trim first few lines to ignore different CPU arch
+# remove graphs for TPAC output
 
-/usr/bin/tail -n +8 $2 | diff - $1
+/usr/bin/tail -n +8 $2 | /usr/bin/head -52 - | /usr/bin/diff - $1
